aafa37fba6a4beea442fe94e051f4e7803f40e1f
KeithAsazawa <keith.asazawa@gmail.com>
2013-09-03 23:41:52 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'jquery-rails'
+gem 'devise'
+gem 'bootstrap-sass'
+gem 'simple_form'
+gem 'aws-sdk'
+gem 'hirb'
+gem 'bootstrap-will_paginate', '0.0.6'
+gem 'paperclip', '~> 3.0'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+	gem 'sqlite3'
+end
+
+group :production do
+	gem 'pg'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fastergram::Application.config.secret_token = 'c3723a2be19c02eca4fe99a0d36f437806285e34b0c492dc9fc8b0ecef86d5b9b4a7679d672d6d18d8cd172e7cc7bfc88c74d43001865dc8c60f0795f10fbba2'
